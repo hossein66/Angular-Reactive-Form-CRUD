b0e99f478fb925363bcc348aef408df4fe95756e
Navabi-m <navabi.hossein@gmail.com>
2023-06-17 20:47:27 +0330

    add phone number validator

---
+++ src/app/modules/customers/validators/phone-number.validator.ts
@@ -0,0 +1,10 @@
+import { AbstractControl, ValidatorFn } from "@angular/forms";
+import { CustomersService } from "../services/customers.service";
+
+    export function PhoneNumberValidator(customerService: CustomersService): ValidatorFn {
+      return (control: AbstractControl ): { [key: string]: any  } | null  => {
+        return customerService.isValidPhoneNumber(control.value.countryCode,control.value.number) ? 
+        { phoneNumberError : true } : null;
+      };
+    }
+  
